0c4989e220c78743e92a2bf9836c9f7a4e9daeed
Brainhimself <adeyemoakin.a@gmail.com>
2024-09-06 22:15:16 +0100

    Add files via upload

--- completeCalculator.py
+++ completeCalculator.py
@@ -2,21 +2,24 @@ number1 = int(input("Enter your first number: "))
 operator = input("Enter your operator (+, -, /, *): ")
 number2 = int(input("Enter your second number: "))
 
-if operator == '+' :
-    result = number1 + number2
-    print(f"The result for {number1} + {number2} is {result}")
+def myCal():
+    if operator == '+' :
+        result = number1 + number2
+        print(f"The result for {number1} + {number2} is {result}")
+    
+    elif operator == '-' :
+        result = number1 - number2
+        print(f"The result for {number1} - {number2} is {result}")
+    
+    elif operator == '*' :
+        result = number1 * number2
+        print(f"The result for {number1} * {number2} is {result}")
+    
+    elif operator == '/' :
+        result = number1 / number2
+        print(f"The result for {number1} / {number2} is {result}")
+    
+    else:
+        print("Operator nor valid, kindly use either of (*, -, + or /)")
 
-elif operator == '-' :
-    result = number1 - number2
-    print(f"The result for {number1} - {number2} is {result}")
-
-elif operator == '*' :
-    result = number1 * number2
-    print(f"The result for {number1} * {number2} is {result}")
-
-elif operator == '/' :
-    result = number1 / number2
-    print(f"The result for {number1} / {number2} is {result}")
-
-else:
-    print("Operator nor valid, kindly use either of (*, -, + or /)")
+myCal()
